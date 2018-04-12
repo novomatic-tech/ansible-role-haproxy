bf12340d47aac57211e772802337092ee94bb469
Paweł Krupa <paulfantom@gmail.com>
2018-04-12 09:38:32 +0200

    Better CI with molecule (#3)
    [fix]
    * add molecule tests; lint role
    
    * add molecule tests; lint role
    
    * remove swp file; fix tests
    
    * remove trailing whitespace

---
+++ tests/test_default.py
@@ -0,0 +1,40 @@
+import pytest
+from testinfra.utils.ansible_runner import AnsibleRunner
+
+testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
+
+
+@pytest.mark.parametrize("dirs", [
+    "/etc/haproxy",
+    "/etc/haproxy/errors",
+    "/run/haproxy"
+])
+def test_directories(host, dirs):
+    d = host.file(dirs)
+    assert d.is_directory
+    assert d.exists
+
+
+@pytest.mark.parametrize("files", [
+    "/etc/systemd/system/haproxy.service",
+    "/etc/haproxy/haproxy.cfg"
+])
+def test_files(host, files):
+    f = host.file(files)
+    assert f.exists
+    assert f.is_file
+
+
+def test_service(host):
+    s = host.service("haproxy")
+    assert s.is_enabled
+    assert s.is_running
+
+
+def test_socket(host):
+    s = host.socket("tcp://0.0.0.0:80")
+    assert s.is_listening
+
+
+def test_user(host):
+    assert "haproxy" in host.user("haproxy").groups
